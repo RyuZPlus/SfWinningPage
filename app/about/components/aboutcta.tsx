d8f1368d9d18799065f97e3141e0afefd924bad7
Aarón Vera <54691622+RyuZPlus@users.noreply.github.com>
2026-04-06 13:43:11 -0500

    Tercera persona

--- app/about/components/aboutcta.tsx
+++ app/about/components/aboutcta.tsx
@@ -4,7 +4,7 @@ export default function AboutCTA() {
   return (
     <section className="py-20 text-center px-6">
       <h2 className="text-3xl md:text-4xl font-bold text-white mb-6">
-        Explora mis proyectos
+        Explora nuestros proyectos
       </h2>
 
       <Link
